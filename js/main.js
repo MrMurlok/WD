3f04ceca9614cf1c53de581faa6bb6a243736f28
MrMurlok <djmurlok.sf@gmail.com>
2025-12-29 14:50:32 +0700

    primary commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var imagebox = document.querySelectorAll('.materialboxed');
+    var instances_imagebox = M.Materialbox.init(imagebox);
+});
